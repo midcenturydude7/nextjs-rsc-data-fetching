4e78891c6ccba18b014842cdabe725f279028b1e
midCenturyDude7 <mgriffes@gmail.com>
2024-02-19 16:35:43 -0800

    Debounced client-side search; project complete

--- app/components/SearchInput.tsx
+++ app/components/SearchInput.tsx
@@ -7,6 +7,8 @@ import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
 export default function SearchInput({ search }: { search?: string }) {
   const router = useRouter();
   const [isPending, startTransition] = React.useTransition();
+  const [timeoutId, setTimeoutId] = React.useState<NodeJS.Timeout>();
+  const isSearching = timeoutId || isPending;
 
   return (
     <div className="relative mt-1 rounded-md shadow-sm">
@@ -20,20 +22,27 @@ export default function SearchInput({ search }: { search?: string }) {
         type="text"
         name="search"
         id="search"
-        className="block w-full rounded-md border-gray-300 pl-10 text-sm text-black focus:border-gray-400 focus:outline-none focus:ring-0"
+        className="block w-full rounded-md border border-gray-300 px-2 py-2 pl-10 text-sm text-black focus:border-gray-400 focus:outline-none focus:ring-0"
         placeholder="Search"
         defaultValue={search}
         onChange={(e) => {
-          startTransition(() => {
-            if (e.target.value) {
-              router.push(`/?search=${e.target.value}`);
-            } else {
-              router.push("/");
-            }
-          });
+          clearTimeout(timeoutId);
+          const id = setTimeout(() => {
+            startTransition(() => {
+              if (e.target.value) {
+                router.push(`/?search=${e.target.value}`);
+              } else {
+                router.push("/");
+              }
+
+              setTimeoutId(undefined);
+            });
+          }, 500);
+
+          setTimeoutId(id);
         }}
       />
-      {isPending && (
+      {isSearching && (
         <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
           <Spinner
             className="h-5 w-5 animate-spin text-gray-400"
